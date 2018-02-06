dfc0ee5626e25bebabab7c14c3813d98bda41fb0
WilliamMena <williammena1991@gmail.com>
2018-02-05 19:52:29 -0500

    Adjusted Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:index, :create, :show, :update, :destroy]
     resources :sessions, only: [:index, :create, :show, :update] do
-      resources :players, only: [:index, :create, :show]
+      resources :players, only: [:index, :create]
     end
-    resources :players, only: [:index]
   end
 end
