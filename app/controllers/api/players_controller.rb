cf6fd309b9f6e10b4eac4ebfffec85bdbbe2fc0d
WilliamMena <williammena1991@gmail.com>
2018-02-05 23:19:28 -0500

    Can create players and assign to a session through the api

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -1,5 +1,18 @@
 class Api::PlayersController < ApplicationController
 
+  def create
+    player = Player.new(player_params)
+    if player.save
+      render json: player
+    else
+      render json: { message: player.errors}, status: 400
+    end
+  end
 
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :score, :session_id)
+  end
 
 end
