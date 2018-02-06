f9b99649b66c0b8310830c0181117217e223c629
WilliamMena <williammena1991@gmail.com>
2018-02-05 20:31:22 -0500

    Can create session, basic way

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -8,4 +8,13 @@ class Api::SessionsController < ApplicationController
     render json: Session.find(params[:id])
   end
 
+  def create
+    session = Session.new
+    if session.save
+      render json: session
+    else
+      render json: { message: session.errors}, status: 400
+    end
+  end
+
 end
