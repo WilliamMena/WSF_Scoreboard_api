c4dd1d874649775468279011f7742f15bb5eb609
WilliamMena <williammena1991@gmail.com>
2018-02-05 20:27:36 -0500

    Can create users from the api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,4 +8,20 @@ class Api::UsersController < ApplicationController
     render json: User.find(params[:id])
   end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: { message: user.errors}, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+
+
 end
